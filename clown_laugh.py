067a849a719ab7fb65f35679adc8f06288c20d51
hmgJak <130262196+hmgJak@users.noreply.github.com>
2023-05-06 20:27:24 -0400

    Create clown_laugh.py

---
+++ clown_laugh.py
@@ -0,0 +1,22 @@
+import pyaudio
+import wave
+
+# Set up audio player
+player = pyaudio.PyAudio()
+sound_file = wave.open("clown_laugh.wav", 'rb')
+stream = player.open(format=player.get_format_from_width(sound_file.getsampwidth()),
+                     channels=sound_file.getnchannels(),
+                     rate=sound_file.getframerate(),
+                     output=True)
+
+# Monitor microphone state
+previous_state = True  # Assume microphone is initially unmuted
+while True:
+    current_state = # Code to check current microphone state (e.g. using system APIs)
+    if current_state and not previous_state:
+        # Microphone was just unmuted, play sound
+        data = sound_file.readframes(1024)
+        while data:
+            stream.write(data)
+            data = sound_file.readframes(1024)
+    previous_state = current_state
