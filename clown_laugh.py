4b75f8be3d5a9c9e4422619604275e02dc138f47
hmgJak <130262196+hmgJak@users.noreply.github.com>
2023-05-06 20:43:17 -0400

    Update clown_laugh.py

--- clown_laugh.py
+++ clown_laugh.py
@@ -1,9 +1,10 @@
 import pyaudio
 import wave
+import sounddevice as sd
 
 # Set up audio player
 player = pyaudio.PyAudio()
-sound_file = wave.open("creepy-clown-laugh--By-tuna.voicemod.net.mp3", 'rb')
+sound_file = wave.open("creepy-clown-laugh--By-tuna.voicemod.mp3", 'rb')
 stream = player.open(format=player.get_format_from_width(sound_file.getsampwidth()),
                      channels=sound_file.getnchannels(),
                      rate=sound_file.getframerate(),
@@ -12,7 +13,7 @@ stream = player.open(format=player.get_format_from_width(sound_file.getsampwidth
 # Monitor microphone state
 previous_state = True  # Assume microphone is initially unmuted
 while True:
-    current_state = # Code to check current microphone state (e.g. using system APIs)
+    current_state = not sd.query_devices('default')['default_mic_mute']  # Check current microphone state
     if current_state and not previous_state:
         # Microphone was just unmuted, play sound
         data = sound_file.readframes(1024)
