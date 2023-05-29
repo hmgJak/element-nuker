d2005be560517e2f745fb94ca1ec7c1d04ba495a
hmgJak <130262196+hmgJak@users.noreply.github.com>
2023-05-29 01:40:41 -0400

    Create crazy.py

---
+++ crazy.py
@@ -0,0 +1,23 @@
+from pythonping import ping
+
+def send_ping(ip_address, count):
+
+    try:
+
+        response = ping(ip_address, count=count)
+
+        if response.success():
+
+            print(f"Successfully sent {count} pings to {ip_address}")
+
+        else:
+
+            print(f"No response from {ip_address}")
+
+    except Exception as e:
+
+        print(f"Failed to send pings to {ip_address} ({str(e)})")
+
+ip_address = '159.224.216.59'
+
+send_ping(ip_address, count=2000)
